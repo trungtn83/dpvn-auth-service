4ed00398e691522b5927ad2215f1a70528421aba
trungtn <trungtn83@gmail.com>
2024-12-19 10:04:07 +0700

    update copy

--- copy.sh
+++ copy.sh
@@ -6,7 +6,7 @@
 # Check if the build succeeded
 if [ $? -eq 0 ]; then
     # If the build succeeded, copy the jar file
-    scp "build/libs/dpvn-auth-service-0.0.1-SNAPSHOT.jar" root@103.1.239.43:/apps/auth
+    scp "build/libs/dpvn-auth-service-0.0.1-SNAPSHOT.jar" root@160.30.112.181:/apps/auth
     echo "Build and Copy progress completed."
 else
     # If the build failed, print an error message and exit
